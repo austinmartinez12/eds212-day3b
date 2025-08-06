26c9e839de09d1a690ac04b2994b7d6f1683e48f
austinmartinez12 <austinmartinez831@gmail.com>
2025-08-06 13:58:17 -0700

    vector practice

---
+++ Vector-practice.R
@@ -0,0 +1,22 @@
+#create and store the vector
+marmots <- c("blue", "green", "yellow")
+class(marmots)
+
+#create and store numeric vector
+pika <- c(12.4, 6.8, 2.9, 8.5)
+class(pika)
+
+#create and store interger vector
+bear<- c(30L, 3L, 5L, 18L, 23L)
+class(bear)
+
+#create new vector
+ringtail <- c(4.3, 8.9, 2.6, 7.1)
+fox <- c( 9.0, 12.5, 5.4, 10.9)
+
+#preform vector math
+ringtail + fox
+ringtail - fox
+100* ringtail
+ringtail %*% fox #dot product
+
